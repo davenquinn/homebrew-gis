c3d7b0f6570f33825c3ee2a1f80a0c8cc04d8177
Daven Quinn <dev@davenquinn.com>
2015-05-27 15:33:12 -0700

    Fixed bugs in mdenoise formula

--- mdenoise.rb
+++ mdenoise.rb
@@ -1,12 +1,14 @@
-class MDenoise < Formula
+class Mdenoise < Formula
   desc "Formula for Xianfang Sun's denoising algorithm, which can be applied to DEMs and other noisy point data"
   homepage "http://www.cs.cf.ac.uk/meshfiltering/index_files/Page342.htm"
   url "http://www.cs.cf.ac.uk/meshfiltering/index_files/Doc/mdsource.zip"
 
+  version "1.0"
+
   sha256 "d0bf1276b8a411e23e2acce94902b40a90b8020385486ca2472ab2cb788d27c9"
 
   def install
-    system "g++", "-o mdenoise","mdenoise.cpp","triangle.c"
+    system "g++", "-o","mdenoise","mdenoise.cpp","triangle.c"
     bin.install "mdenoise"
   end
 end
