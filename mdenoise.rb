ffee2125e5f49e5d80a0b59558c1ecd41d276486
Daven Quinn <dev@davenquinn.com>
2015-05-27 15:21:19 -0700

    Added formula for mdenoise

---
+++ mdenoise.rb
@@ -0,0 +1,12 @@
+class MDenoise < Formula
+  desc "Formula for Xianfang Sun's denoising algorithm, which can be applied to DEMs and other noisy point data"
+  homepage "http://www.cs.cf.ac.uk/meshfiltering/index_files/Page342.htm"
+  url "http://www.cs.cf.ac.uk/meshfiltering/index_files/Doc/mdsource.zip"
+
+  sha256 "d0bf1276b8a411e23e2acce94902b40a90b8020385486ca2472ab2cb788d27c9"
+
+  def install
+    system "g++", "-o mdenoise","mdenoise.cpp","triangle.c"
+    bin.install "mdenoise"
+  end
+end
